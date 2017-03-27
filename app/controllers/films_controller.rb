ae98636d0778dab42618ef9e5dcb8127b01291cd
jonathan <zhanggongzifei@163.com>
2017-03-27 18:46:30 +0800

    add

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -6,4 +6,18 @@ class FilmsController < ApplicationController
   def new
     @film = Film.new
   end
+
+  def create
+    @film = Film.new(film_params)
+    @film.save
+
+    redirect_to films_path
+  end
+
+  private
+
+  def film_params
+    params.require(:film).permit(:title, :description)
+  end
+
 end
